4e90468b5bf700547f863476c59663e4029fc7d0
vinu <ksavinu121@gmail.com>
2025-09-10 19:52:44 +0530

    error correction in user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -22,11 +22,10 @@ export const registerUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = await User.create({ name, email, password: hashedPassword });
-    const token = generateToken(user._id, res);
+    generateToken(user._id, res);
     res.status(201).json({
       message: "User created successfully",
       data: { id: user._id, name: user.name, email: user.email },
-      token: token,
     });
   } catch (error) {
     console.log("Error creating user", error.message);
@@ -53,14 +52,11 @@ export const loginUser = async (req, res) => {
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
-    const token = generateToken(user._id, res);
-    res
-      .status(200)
-      .json({
-        message: "User logged in successfully",
-        data: { id: user._id, name: user.name, email: user.email },
-        token: token,
-      });
+    generateToken(user._id, res);
+    res.status(200).json({
+      message: "User logged in successfully",
+      data: { id: user._id, name: user.name, email: user.email },
+    });
   } catch (error) {
     console.log("Error logging in user", error.message);
     res.status(500).json({ message: "Internal server error" });
